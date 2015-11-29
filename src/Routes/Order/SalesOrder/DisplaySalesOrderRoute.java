cbee1d6c34e39f5c8e789b7226948f4425df9682
Bruna Koch Schmitt <brunak_s@hotmail.com>
2015-11-29 17:23:26 -0200

    Refactoring Create Sales Order

--- src/Routes/Order/SalesOrder/DisplaySalesOrderRoute.java
+++ src/Routes/Order/SalesOrder/DisplaySalesOrderRoute.java
@@ -1,12 +1,30 @@
 package Routes.Order.SalesOrder;
 
+import Entities.Order.SalesOrderInfo;
 import Interfaces.Persistence.SalesOrderRepository;
+import com.google.gson.Gson;
+import spark.Request;
+import spark.Response;
 import spark.Route;
 
 /**
  * Created by Bruna Koch Schmitt on 29/11/2015.
  */
 public class DisplaySalesOrderRoute implements Route {
+
+    private SalesOrderRepository salesOrderRepository;
+    private Gson converter = new Gson();
+
     public DisplaySalesOrderRoute(SalesOrderRepository salesOrderRepository) {
+        this.salesOrderRepository = salesOrderRepository;
+    }
+
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        return converter.toJson(makeSalesOrderInfo(request));
+    }
+
+    private SalesOrderInfo makeSalesOrderInfo(Request request) {
+        return salesOrderRepository.getById(request.queryString());
     }
 }
